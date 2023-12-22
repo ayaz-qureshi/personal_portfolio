87bf606a6def18c3828ec86fa5688287cb667569
ayazpro <80163535+ayazpro@users.noreply.github.com>
2023-12-22 11:07:57 +0500

    slight changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div name="home"  className='bg-gradient-to-t from-black to-slate-900  flex justify-center items-center min-h-screen pt-24'>
       <div className='flex flex-col md:flex-row w-2/3 items-center md:items-start'>
         <div className='flex items-start justify-start flex-col text-white '>
-          <h2 className='text-5xl font-bold mb-7'>I'm a React.js Developer</h2>
+          <h2 className='text-5xl font-bold mb-7'>I'm a React.js Front End Developer</h2>
           <p>As a junior React.js developer, I am proficient in building modern, responsive user interfaces using React.js.</p>
           <div className='group flex items-center bg-blue-800 p-2 mt-5 sm:mt-20 rounded-sm'>
             <button className='text-left'>Portfolio </button><span className='group-hover:rotate-90 ml-1 duration-200'><IoIosArrowForward size={14} /></span>
